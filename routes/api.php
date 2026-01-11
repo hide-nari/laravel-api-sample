37c9b32c0782b905b6a8f5fd179fab0c1008e533
watanabeHi <hide3@mild.ocn.ne.jp>
2026-01-11 09:35:01 +0900

    Add update and delete logic

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,31 @@ Route::get('/people', function () {
     return Person::all();
 });
 
+Route::post('/person/store', function (Request $request) {
+    return Person::create([
+        'name' => $request->name,
+        'age'  => $request->age,
+    ]);
+});
+
 Route::get('/person/{person}', function (Person $person) {
     return $person;
+});
+
+Route::post('/person/{person}', function (Person $person, Request $request) {
+    $person->update([
+        'name' => $request->name,
+        'age'  => $request->age,
+    ]);
+});
+
+Route::post('/person/store', function (Request $request) {
+    return Person::create([
+        'name' => $request->name,
+        'age'  => $request->age,
+    ]);
+});
+
+Route::get('/person/delete/{person}', function (Person $person) {
+    $person->delete();
 });
